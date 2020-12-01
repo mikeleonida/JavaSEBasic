a1b7d24590d9be9145d26779d4c43378323d649a
unknown <michael.leonida@gmail.com>
2020-12-01 13:22:23 -0500

    Additional attempt to commit

---
+++ src/DisplaySquareAndCube.java
@@ -0,0 +1,14 @@
+
+public class DisplaySquareAndCube {
+
+	public static void main(String[] args) {
+		int num = 10;
+		int numSquared = num*num;
+		int numCubed = num*num*num;
+		
+		System.out.println("Square of 10 is: " + numSquared);
+		System.out.println("Cube of 10 is: " + numCubed);
+
+	}
+
+}
